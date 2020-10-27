6d9ac7dd231e80c37d006ce33e23852975dc757a
Filip Naumovski <filip.naumovski00@live.com>
2020-10-27 12:46:09 +0100

    Fix text popup in Gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -117,46 +117,48 @@ function Gallery() {
     }
 
     return (
-        <motion.div initial={{ scale: 1, opacity: 1, x: -2000 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: '1.2' }}>
-            <table>
-                <tbody>
-                    <motion.h2 animate={{ scale: 1.5, y: -500, opacity: 0 }} transition={{ duration: '7' }} className="gallery-popup">Try double clicking the pictures!</motion.h2>
-                    <img src='https://picsum.photos/seed/picsum/4000' alt="" className="images-inv" onLoad={imageLoaded("image1")} />
-                    <img src='https://picsum.photos/seed/pics/4000' alt="" className="images-inv" onLoad={imageLoaded("image2")} />
-                    <tr>
-                        <td style={{ textAlign: "left" }} className="gallery-element">
-                            <div>
+        <div>
+            <motion.h2 animate={{ scale: 1.5, y: -500, opacity: 0, }} transition={{ duration: '7' }} className="gallery-popup">Try double clicking the pictures!</motion.h2>
+            <motion.div initial={{ scale: 1, opacity: 1, x: -2000 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: '1.2' }}>
+                <table>
+                    <tbody>
+                        <img src='https://picsum.photos/seed/picsum/4000' alt="" className="images-inv" onLoad={imageLoaded("image1")} />
+                        <img src='https://picsum.photos/seed/seed2/4000' alt="" className="images-inv" onLoad={imageLoaded("image2")} />
+                        <tr>
+                            <td style={{ textAlign: "left" }} className="gallery-element">
                                 <div>
-                                    {state.loaded1 ? <img src='https://picsum.photos/seed/picsum/4000' alt="" className="images" onDoubleClick={handleClickImg("image1")} /> : <img alt="" src={loading} className="loading" />}
                                     <div>
-                                        <img src={heart} alt="" className={imageClicked.image1 ? "heart-showing" : "heart"} />
+                                        {state.loaded1 ? <img src='https://picsum.photos/seed/picsum/4000' alt="" className="images" onDoubleClick={handleClickImg("image1")} /> : <img alt="" src={loading} className="loading" />}
+                                        <div>
+                                            <img src={heart} alt="" className={imageClicked.image1 ? "heart-showing" : "heart"} />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className="button-img" onClick={e => handleClick(e)} value="button1">Like</button>
+                                        <h3 style={{ fontSize: 30 }}><img src={thumbsUp} alt="" width="30px" height="30px"></img> {state.likes1}</h3>
                                     </div>
                                 </div>
-                                <div>
-                                    <button className="button-img" onClick={e => handleClick(e)} value="button1">Like</button>
-                                    <h3 style={{ fontSize: 30 }}><img src={thumbsUp} alt="" width="30px" height="30px"></img> {state.likes1}</h3>
-                                </div>
-                            </div>
-                        </td>
+                            </td>
 
-                        <td style={{ textAlign: "left" }} className="gallery-element">
-                            <div>
+                            <td style={{ textAlign: "left" }} className="gallery-element">
                                 <div>
-                                    {state.loaded2 ? <img src='https://picsum.photos/seed/pics/4000' alt="" className="images" onDoubleClick={handleClickImg("image2")} /> : <img alt="" src={loading} className="loading" />}
                                     <div>
-                                        <img src={heart} alt="" className={imageClicked.image2 ? "heart-showing" : "heart"} />
+                                        {state.loaded2 ? <img src='https://picsum.photos/seed/seed2/4000' alt="" className="images" onDoubleClick={handleClickImg("image2")} /> : <img alt="" src={loading} className="loading" />}
+                                        <div>
+                                            <img src={heart} alt="" className={imageClicked.image2 ? "heart-showing" : "heart"} />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className="button-img" onClick={e => handleClick(e)} value="button2">Like</button>
+                                        <h3 style={{ fontSize: 30 }}><img src={thumbsUp} alt="" width="30px" height="30px"></img> {state.likes2}</h3>
                                     </div>
                                 </div>
-                                <div>
-                                    <button className="button-img" onClick={e => handleClick(e)} value="button2">Like</button>
-                                    <h3 style={{ fontSize: 30 }}><img src={thumbsUp} alt="" width="30px" height="30px"></img> {state.likes2}</h3>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table >
-        </motion.div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table >
+            </motion.div>
+        </div>
     )
 }
 

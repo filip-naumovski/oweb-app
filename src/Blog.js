b6d3ee866e3ae7bb927438dad7f4543fe97f9f8e
Filip Naumovski <filip.naumovski00@live.com>
2020-12-16 00:05:35 +0100

    Change Blog posts to be stored on local SQL server

--- src/Blog.js
+++ src/Blog.js
@@ -1,139 +1,194 @@
-import "./style.css"
-import React, { useState, useEffect, useRef } from "react"
-import { motion } from "framer-motion"
+import "./style.css";
+import React, { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 
 function Blog() {
-	const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-	const nameRef = useRef(null)
-	const contentRef = useRef(null)
-	const titleRef = useRef(null)
+  const nameRef = useRef(null);
+  const contentRef = useRef(null);
+  const titleRef = useRef(null);
 
-	const [currentPost, setCurrentPost] = useState({
-		name: "",
-		title: "",
-		content: "",
-		date: "",
-	})
+  useEffect(() => {
+    fetch("http://localhost:53771/api/BlogPosts", {
+      mode: "cors",
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .then(() => {
+        setPosts((prevState) => {
+          const newState = [...prevState];
+          prevState.forEach((element) => {
+            const newDate = Date(element.date);
+            element.date = newDate.toString();
+          });
+          return newState;
+        });
+      });
+  }, []);
 
-	const [clicked, setClicked] = useState(false)
+  const [currentPost, setCurrentPost] = useState({
+    name: "",
+    title: "",
+    content: "",
+    date: "",
+    id: 0,
+  });
 
-	const leadingZero = (time) => {
-		if (time >= 0 && time <= 9) {
-			time = "0" + time
-		}
-		return time
-	}
+  const currentDate = new Date();
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		const currentDate = new Date()
-		let hours = currentDate.getHours().toString()
-		hours = leadingZero(hours)
-		let minutes = currentDate.getMinutes().toString()
-		minutes = leadingZero(minutes)
-		let seconds = currentDate.getSeconds().toString()
-		seconds = leadingZero(seconds)
-		const newDateString =
-			currentDate.getDate() +
-			"/" +
-			parseInt(currentDate.getMonth() + 1) +
-			"/" +
-			currentDate.getFullYear() +
-			" " +
-			hours +
-			":" +
-			minutes +
-			":" +
-			seconds
+  const [clicked, setClicked] = useState(false);
 
-		setPosts((prevState) => {
-			const newState = [...prevState]
-			const newObj = {
-				name: nameRef.current.value,
-				title: titleRef.current.value,
-				content: contentRef.current.value,
-				date: newDateString,
-			}
-			newState.push(newObj)
-			return newState
-		})
-		setClicked(true)
-		setTimeout(() => {
-			setClicked(false)
-		}, 100)
-	}
+  const leadingZero = (time) => {
+    if (time >= 0 && time <= 9) {
+      time = "0" + time;
+    }
+    return time;
+  };
 
-	const handleChange = (e) => {
-		const value = e.target.value
-		const name = e.target.name
-		setCurrentPost((prevState) => {
-			const newState = {
-				...prevState,
-			}
-			newState[name] = value
-			return newState
-		})
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-	return (
-		<motion.div
-			initial={{ scale: 1, opacity: 0 }}
-			animate={{ opacity: 1, x: 0 }}
-			transition={{ duration: "1.2" }}>
-			<h1
-				className="blogHeader"
-				style={{
-					fontSize: "60px",
-					textAlign: "center",
-					fontFamily: "sans-serif",
-				}}>
-				BLOG
-			</h1>
-			<div className="blogDiv">
-				<div className="formDiv">
-					<form onSubmit={(e) => handleSubmit(e)}>
-						<input
-							ref={nameRef}
-							type="text"
-							name="name"
-							placeholder="Name"
-							value={currentPost.name}
-							onChange={(e) => handleChange(e)}
-						/>
-						<input
-							ref={titleRef}
-							type="text"
-							name="title"
-							placeholder="Title"
-							value={currentPost.title}
-							onChange={(e) => handleChange(e)}
-						/>
-						<textarea
-							ref={contentRef}
-							name="content"
-							placeholder="Share something!"
-							value={currentPost.content}
-							onChange={(e) => handleChange(e)}
-						/>
-						<button className="button" type="submit">
-							Submit
-						</button>
-					</form>
-				</div>
-				<div className="blog">
-					{posts.map((post, key) => (
-						<div className={clicked ? "blogPostClicked" : "blogPost"} key={key}>
-							<h1>{post.title}</h1>
-							<h3>Date: {post.date}</h3>
-							<p>{post.content}</p>
-							<h3 className="signature">{post.name}</h3>
-						</div>
-					))}
-				</div>
-			</div>
-		</motion.div>
-	)
+    fetch("http://localhost:53771/api/BlogPosts", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: currentPost.name,
+        title: currentPost.title,
+        content: currentPost.content,
+        date: currentDate.toISOString(),
+      }),
+    }).then(() => {
+      setPosts((prevState) => {
+        const newState = [...prevState];
+        const newId =
+          newState.length != 0 ? newState[newState.length - 1].id + 1 : 1;
+        const newObj = {
+          name: currentPost.name,
+          title: currentPost.title,
+          content: currentPost.content,
+          date: currentDate.toString(),
+          id: newId,
+        };
+        newState.push(newObj);
+        return newState;
+      });
+      setClicked(true);
+      setTimeout(() => {
+        setClicked(false);
+      }, 100);
+    });
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setCurrentPost((prevState) => {
+      const newState = {
+        ...prevState,
+      };
+      newState[name] = value;
+      return newState;
+    });
+  };
+
+  const handleDelete = (cId) => {
+    const id = cId;
+    console.log(id);
+    fetch("http://localhost:53771/api/BlogPosts/" + id, {
+      mode: "cors",
+      method: "DELETE",
+    }).then(() => {
+      setPosts((prevState) => {
+        const newState = [...prevState];
+        const indexToRemove = newState.findIndex((x) => x.id === id);
+        newState.splice(indexToRemove, 1);
+        console.log(newState);
+        return newState;
+      });
+      setClicked(true);
+      setTimeout(() => {
+        setClicked(false);
+      }, 100);
+    });
+  };
+
+  return (
+    <motion.div
+      initial={{ scale: 1, opacity: 0 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: "1.2" }}
+    >
+      <h1
+        className="blogHeader"
+        style={{
+          fontSize: "60px",
+          textAlign: "center",
+          fontFamily: "sans-serif",
+        }}
+      >
+        BLOG
+      </h1>
+      <div className="blogDiv">
+        <div className="formDiv">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+              ref={nameRef}
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={currentPost.name}
+              onChange={(e) => handleChange(e)}
+            />
+            <input
+              ref={titleRef}
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={currentPost.title}
+              onChange={(e) => handleChange(e)}
+            />
+            <textarea
+              ref={contentRef}
+              name="content"
+              placeholder="Share something!"
+              value={currentPost.content}
+              onChange={(e) => handleChange(e)}
+            />
+            <button className="button" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div className="blog">
+          {posts.map((post, key) => {
+            const currentId = post.id;
+            return (
+              <div
+                className={clicked ? "blogPostClicked" : "blogPost"}
+                key={post.id}
+              >
+                <h1>{post.title}</h1>
+                <h3>{post.date}</h3>
+                <br></br>
+                <p>{post.content}</p>
+                <button onClick={() => handleDelete(currentId)}>Delete</button>
+                <h3 className="signature">{post.name}</h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 
-export default Blog
+export default Blog;
--- src/Survey.js
+++ src/Survey.js
@@ -1,163 +1,176 @@
-import "./style.css"
-import React, { useState, useEffect } from "react"
-import { motion } from "framer-motion"
+import "./style.css";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 function Survey() {
-	const [state, setState] = useState({
-		"first name": "",
-		"last name": "",
-		liked: false,
-		rating: 1,
-		comment: "",
-		gender: "",
-	})
+  const [state, setState] = useState({
+    "first name": "",
+    "last name": "",
+    liked: false,
+    rating: 1,
+    comment: "",
+    gender: "",
+  });
 
-	const [submitted, setSubmitted] = useState(false)
+  const [submitted, setSubmitted] = useState(false);
 
-	const handleChange = (test) => (e) => {
-		const name = e.target.name
-		const type = e.target.type
-		const value = e.target.value
-		if (type !== "checkbox") {
-			setState((prevState) => {
-				const newState = {
-					...prevState,
-				}
-				newState[name] = value
-				return newState
-			})
-			if (name === "rating") {
-				setState((prevState) => {
-					const newState = {
-						...prevState,
-					}
-					const tooHigh = newState[name] > 5 ? true : false
-					const tooLow = newState[name] < 1 ? true : false
-					if (tooHigh === true) {
-						newState[name] = 5
-					}
-					if (tooLow === true) {
-						newState[name] = 1
-					}
-					return newState
-				})
-			}
-		} else {
-			setState((prevState) => {
-				const newState = {
-					...prevState,
-				}
-				newState[name] = !newState[name]
-				return newState
-			})
-		}
-	}
+  const handleChange = (test) => (e) => {
+    const name = e.target.name;
+    const type = e.target.type;
+    const value = e.target.value;
+    if (type !== "checkbox") {
+      setState((prevState) => {
+        const newState = {
+          ...prevState,
+        };
+        newState[name] = value;
+        return newState;
+      });
+      if (name === "rating") {
+        setState((prevState) => {
+          const newState = {
+            ...prevState,
+          };
+          const tooHigh = newState[name] > 5 ? true : false;
+          const tooLow = newState[name] < 1 ? true : false;
+          if (tooHigh === true) {
+            newState[name] = 5;
+          }
+          if (tooLow === true) {
+            newState[name] = 1;
+          }
+          return newState;
+        });
+      }
+    } else {
+      setState((prevState) => {
+        const newState = {
+          ...prevState,
+        };
+        newState[name] = !newState[name];
+        return newState;
+      });
+    }
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		setSubmitted(true)
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
 
-	return (
-		<div>
-			<h1 className={submitted ? "surveyPopup" : "surveyPopupHidden"}>Thanks for submitting!</h1>
-			<motion.div
-				initial={{ scale: 1, opacity: 1, y: 1000 }}
-				animate={{ opacity: 1, y: 0 }}
-				transition={{ duration: "1.2" }}>
-				<table className="submitted-table" align="center">
-					{!submitted ? (
-						<tr>
-							<td>
-								<h1 style={{ fontSize: 40 }}>Survey:</h1>
-								<form name="submit" className="surveyForm" onSubmit={(e) => handleSubmit(e)}>
-									<input
-										type="text"
-										placeholder="First name"
-										value={state.name}
-										onChange={handleChange()}
-										name="first name"
-										required
-									/>
-									<input
-										type="text"
-										placeholder="Last name"
-										value={state.surname}
-										onChange={handleChange()}
-										name="last name"
-										required
-									/>
-									<div className="genderDiv" onChange={handleChange()}>
-										<div>
-											<label htmlFor="male">Male</label>
-											<input type="radio" value="Male" name="gender" required />
-										</div>
-										<div>
-											<label htmlFor="female">Female</label>
-											<input type="radio" value="Female" name="gender" required />
-										</div>
-									</div>
-									<div style={{ display: "flex" }}>
-										<h3>Did you like the site?</h3>
-										<input
-											style={{ width: "20px", marginTop: "4px" }}
-											id="liked"
-											type="checkbox"
-											value={state.liked}
-											onChange={handleChange()}
-											name="liked"
-											required
-										/>
-									</div>
-									<div style={{ display: "flex" }}>
-										<h3>Rating: </h3>
-										<input
-											style={{ width: "25px" }}
-											type="number"
-											min="1"
-											max="5"
-											value={state.rating}
-											onChange={handleChange()}
-											name="rating"
-										/>
-									</div>
-									<textarea
-										className="surveyComment"
-										placeholder="Any additional comments?"
-										value={state.comment}
-										onChange={handleChange()}
-										name="comment"
-									/>
-									<button type="submit" className="button-comment-survey">
-										Submit
-									</button>
-								</form>
-							</td>
-						</tr>
-					) : (
-						<div>
-							<motion.div
-								initial={{ scale: 1, opacity: 1, y: -1000, zIndex: -999 }}
-								animate={{ opacity: 1, y: 0, zIndex: 1 }}
-								transition={{ duration: "1.2" }}>
-								<tr className="submittedTr">
-									<td className="submittedTd">
-										<h1>Name: {state["first name"]}</h1>
-										<h1>Surname: {state["last name"]}</h1>
-										<h1>Gender: {state.gender}</h1>
-										<h1>Rating: {state.rating}</h1>
-										<h1>Liked: {state.liked ? "Yes" : "No"}</h1>
-										<h1>Comment: </h1>
-										<p>{state.comment}</p>
-									</td>
-								</tr>
-							</motion.div>
-						</div>
-					)}
-				</table>
-			</motion.div>
-		</div>
-	)
+  return (
+    <div>
+      <h1 className={submitted ? "surveyPopup" : "surveyPopupHidden"}>
+        Thanks for submitting!
+      </h1>
+      <motion.div
+        initial={{ scale: 1, opacity: 1, y: 1000 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: "1.2" }}
+      >
+        <table className="submitted-table" align="center">
+          {!submitted ? (
+            <tr>
+              <td>
+                <h1 style={{ fontSize: 40 }}>Survey:</h1>
+                <form
+                  name="submit"
+                  className="surveyForm"
+                  onSubmit={(e) => handleSubmit(e)}
+                >
+                  <input
+                    type="text"
+                    placeholder="First name"
+                    value={state.name}
+                    onChange={handleChange()}
+                    name="first name"
+                    required
+                  />
+                  <input
+                    type="text"
+                    placeholder="Last name"
+                    value={state.surname}
+                    onChange={handleChange()}
+                    name="last name"
+                    required
+                  />
+                  <div className="genderDiv" onChange={handleChange()}>
+                    <div>
+                      <label htmlFor="male">Male</label>
+                      <input type="radio" value="Male" name="gender" required />
+                    </div>
+                    <div>
+                      <label htmlFor="female">Female</label>
+                      <input
+                        type="radio"
+                        value="Female"
+                        name="gender"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div style={{ display: "flex" }}>
+                    <h3>Did you like the site?</h3>
+                    <input
+                      style={{ width: "20px", marginTop: "4px" }}
+                      id="liked"
+                      type="checkbox"
+                      value={state.liked}
+                      onChange={handleChange()}
+                      name="liked"
+                      required
+                    />
+                  </div>
+                  <div style={{ display: "flex" }}>
+                    <h3>Rating: </h3>
+                    <input
+                      style={{ width: "25px" }}
+                      type="number"
+                      min="1"
+                      max="5"
+                      value={state.rating}
+                      onChange={handleChange()}
+                      name="rating"
+                    />
+                  </div>
+                  <textarea
+                    className="surveyComment"
+                    placeholder="Any additional comments?"
+                    value={state.comment}
+                    onChange={handleChange()}
+                    name="comment"
+                  />
+                  <button type="submit" className="button-comment-survey">
+                    Submit
+                  </button>
+                </form>
+              </td>
+            </tr>
+          ) : (
+            <div>
+              <motion.div
+                initial={{ scale: 1, opacity: 1, y: -1000, zIndex: -999 }}
+                animate={{ opacity: 1, y: 0, zIndex: 1 }}
+                transition={{ duration: "1.2" }}
+              >
+                <tr className="submittedTr">
+                  <td className="submittedTd">
+                    <h1>Name: {state["first name"]}</h1>
+                    <h1>Surname: {state["last name"]}</h1>
+                    <h1>Gender: {state.gender}</h1>
+                    <h1>Rating: {state.rating}</h1>
+                    <h1>Liked: {state.liked ? "Yes" : "No"}</h1>
+                    <h1>Comment: </h1>
+                    <p>{state.comment}</p>
+                  </td>
+                </tr>
+              </motion.div>
+            </div>
+          )}
+        </table>
+      </motion.div>
+    </div>
+  );
 }
 
-export default Survey
+export default Survey;
